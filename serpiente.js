d22fc5c548b2fa9b9b5cab095a957313710a6749
carlosao24 <carloslopez21trabajos@gmail.com>
2026-05-07 21:20:55 -0500

    Crear cuadrícula

--- serpiente.js
+++ serpiente.js
@@ -1,25 +1,46 @@
 
-    // 1. Capturamos el canvas y su contexto de dibujo
-    const canvas = document.getElementById("canvasJuego");
-    const ctx = canvas.getContext("2d");
+// 1. Capturamos el canvas y su contexto de dibujo
+const canvas = document.getElementById("canvasJuego");
+const ctx = canvas.getContext("2d");
 
+const TAMANIO_CELDA = 25
 
-    
+function dibujarTableroX(){
+  for(let i = 0; i < canvas.width; i+=TAMANIO_CELDA){
+    ctx.strokeStyle = "#5EF527"
+    ctx.beginPath()
+    ctx.moveTo(i,0)
+    ctx.lineTo(i,canvas.height)
+    ctx.stroke()
+  }
+}
 
-    // Primera pintura del juego al cargar la página
-    dibujarTodo();
+function  dibujarTableroY(){
+  for(let i = 0; i < canvas.height; i+=TAMANIO_CELDA){
+    ctx.strokeStyle = "#5EF527"
+    ctx.beginPath()
+    ctx.moveTo(0,i)
+    ctx.lineTo(canvas.width,i)
+    ctx.stroke()
+  }
+}
 
-    // =========================
-    // FUNCIONES DE DIBUJO
-    // =========================
+// Primera pintura del juego al cargar la página
+dibujarTodo();
 
-    function limpiarCanvas() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
+// =========================
+// FUNCIONES DE DIBUJO
+// =========================
 
-    function dibujarTodo() {
-      limpiarCanvas();
-    }
+function limpiarCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function dibujarTodo() {
+  limpiarCanvas()
+  dibujarTableroX()
+  dibujarTableroY()
+}
 
 
 
