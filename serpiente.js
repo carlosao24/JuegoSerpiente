64a4460d60638002e46e1c6303698a02caba57f0
carlosao24 <carloslopez21trabajos@gmail.com>
2026-05-14 18:52:35 -0500

    Taller parte 4

--- serpiente.js
+++ serpiente.js
@@ -5,7 +5,11 @@ const ctx = canvas.getContext("2d");
 let intervaloSerpiente
 let direccionActual = "arriba"
 let puntaje = 0
-let velocidad = 1000
+let nivel = "Nivel 1"
+let velocidad = 800
+let juegoFinalizado = false
+const SONIDO_COMER = new Audio("audio/comer.wav")
+const SONIDO_GAMEOVER = new Audio("audio/gamer-over.mp3")
 
 const TAMANIO_CELDA = 25
 const SERPIENTE = [
@@ -137,23 +141,72 @@ function moverSerpiente(){
   if(direccionActual == "abajo"){
     moverAbajo()
   }
-  let resultado = atrapaComida()
-  if(resultado == true){
-    puntaje +=1
-    document.getElementById("puntaje").innerText = puntaje
-    let cola = SERPIENTE[SERPIENTE.length -1]
-    let nuevaCola = {x: cola.x, y: cola.y}
-    SERPIENTE.push(nuevaCola)
-    generarComida()
+  verificarColisionBorde()
+  if(juegoFinalizado == false){
+    let resultado = atrapaComida()
+    if(resultado == true){
+      puntaje +=1
+      document.getElementById("puntaje").innerText = puntaje
+      let cola = SERPIENTE[SERPIENTE.length -1]
+      let nuevaCola = {x: cola.x, y: cola.y}
+      SERPIENTE.push(nuevaCola)
+      generarComida()
+    }
+    verificarNivel()
   }
   dibujarTodo()
+}
+
+function verificarNivel(){
+  if(puntaje >= 5 && puntaje < 10){
+    verificarColisionBorde()
+    if(puntaje ==5){
+      nivel = "Nivel 2"
+      document.getElementById("estado").innerText = nivel
+    }
+    clearInterval(intervaloSerpiente)
+    velocidad = 450
+    iniciarJuego()
+  }else if(puntaje >= 10 && puntaje < 15){
+    verificarColisionBorde()
+    if(puntaje == 10){
+      nivel = "Nivel 3"
+      document.getElementById("estado").innerText = nivel
+    }
+    clearInterval(intervaloSerpiente)
+    velocidad = 300
+    iniciarJuego()
+  }else if(puntaje >= 15 && puntaje < 20){
+    verificarColisionBorde()
+    if(puntaje == 15){
+      nivel = "Nivel 4"
+      document.getElementById("estado").innerText = nivel
+    }
+    clearInterval(intervaloSerpiente)
+    velocidad = 150
+    iniciarJuego()
+  }else if(puntaje >= 20){
+    verificarColisionBorde()
+    if(puntaje == 20){
+      nivel = "Nivel 5"
+      document.getElementById("estado").innerText = nivel
+    }
+    clearInterval(intervaloSerpiente)
+    velocidad = 50
+    iniciarJuego()
+  }
 }
 
 function cambiarDireccion(nuevaDireccion){
   direccionActual = nuevaDireccion
 }
 function iniciarJuego(){
-intervaloSerpiente = setInterval(moverSerpiente,velocidad)
+  document.getElementById("descripcion").innerText = "Pon a prueba tus habilidades en el clasico Juego de la Serpiente"
+  document.getElementById("estado").innerText = nivel
+  if(juegoFinalizado == true){
+    return
+  }
+  intervaloSerpiente = setInterval(moverSerpiente,velocidad)
 }
 function pausarJuego(){
   clearInterval(intervaloSerpiente)
@@ -173,6 +226,8 @@ function pintarComida(){
 
 function atrapaComida(){
   if(SERPIENTE[0].x == comida.comidaX && SERPIENTE[0].y == comida.comidaY){
+    SONIDO_COMER.currentTime = 0; 
+    SONIDO_COMER.play();
     return true
   }else{
     return false
@@ -180,6 +235,59 @@ function atrapaComida(){
 }
 
 function reiniciarJuego(){
-  location.reload()
+  puntaje = 0
+  SERPIENTE.length = 0
+  SERPIENTE[0] = { x: 5, y: 3 }
+  SERPIENTE[1] = { x: 5, y: 4 }
+  SERPIENTE[2] = { x: 5, y: 5 }
+  SERPIENTE[3] = { x: 5, y: 6 }
+  direccionActual = "arriba"
+  generarComida()
+  habilitarBoton()
+  juegoFinalizado = false
+  document.getElementById("descripcion").innerText = "Estas listo para el desafio"
+  dibujarTodo()
 }
 
+function deshabilitarBoton(){
+  let arriba = document.getElementById("arriba")
+  arriba.disabled = true
+  let izquierda = document.getElementById("izquierda")
+  izquierda.disabled = true
+  let pausa = document.getElementById("pausa")
+  pausa.disabled = true
+  let derecha = document.getElementById("derecha")
+  derecha.disabled = true
+  let abajo = document.getElementById("abajo")
+  abajo.disabled = true
+  let iniciar = document.getElementById("iniciar")
+  iniciar.disabled = true
+}
+
+function habilitarBoton(){
+  let arriba = document.getElementById("arriba")
+  arriba.disabled = false
+  let izquierda = document.getElementById("izquierda")
+  izquierda.disabled = false
+  let pausa = document.getElementById("pausa")
+  pausa.disabled = false
+  let derecha = document.getElementById("derecha")
+  derecha.disabled = false
+  let abajo = document.getElementById("abajo")
+  abajo.disabled = false
+  let iniciar = document.getElementById("iniciar")
+  iniciar.disabled = false
+}
+
+function verificarColisionBorde(){
+  let cabeza = SERPIENTE[0]
+  if( cabeza.x < 0 || cabeza.x >= canvas.width/TAMANIO_CELDA || cabeza.y < 0 || cabeza.y >= canvas.height/TAMANIO_CELDA){
+    SONIDO_GAMEOVER.currentTime = 0; 
+    SONIDO_GAMEOVER.play();
+    document.getElementById("estado").innerText = "Game Over"
+    pausarJuego()
+    document.getElementById("descripcion").innerText = "Mejor suerte la proxima vez"
+    juegoFinalizado = true
+    deshabilitarBoton()
+  }
+}
